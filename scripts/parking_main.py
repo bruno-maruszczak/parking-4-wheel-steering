0d90bdeff7fd8715b561300d0ab3138e46afdf31
Bruno Maruszczak <72678261+bruno-maruszczak@users.noreply.github.com>
2024-05-24 10:16:49 +0000

    Initial commit. Extracted the project from a previous repo.
    Co-authored-by: JinLobana <lobanajin@gmail.com>
    Co-authored-by: danak212 <danak112@wp.pl>

---
+++ scripts/parking_main.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+# local files
+from lib.parking import ParkingLot, Car, ParkingLotBitMap
+from lib.OMPL import OMPL
+from lib.plot import Plot
+
+def main():
+    # parking_lot = ParkingLot()
+    parking_lot_bitmap = ParkingLotBitMap(Path(os.getcwd()).joinpath("data/map/parking_layout.npy"))
+    parking_lot_bitmap.get_obstacles()
+    # print(parking_lot_bitmap.obstacles)
+
+    plot_bounds = parking_lot_bitmap.PLOT_BOUNDS
+    # Debug plotting
+    # parking_lot_bitmap.plot()
+    # parking_lot_bitmap.map_plot(plot_bounds)
+
+    car = Car()
+
+    # parking_lot.init_parking_slots(car.CAR_LEN,car.CAR_WID)
+    ompl = OMPL(plot_bounds, car, parking_lot_bitmap)
+
+    
+    
+    colours = {"bicycle": "deepskyblue", "4ws": "orange"}
+    results = {m: ompl.create_planner(m) for m in ("bicycle", "4ws")}
+
+    plot = Plot(parking_lot_bitmap, results, plot_bounds, colours, car)
+    plot.static_plot(Path(os.getcwd()).joinpath("data/out/parking_paths.png"))
+    plot.animate(Path(os.getcwd()).joinpath("data/out/parking_animation.gif"))
+
+
+if __name__ == "__main__":
+    main()
